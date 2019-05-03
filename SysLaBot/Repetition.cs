275915cda2cab46f84b978ec9f266570e42b0911
skytomo <tomo1kawa.fullerene@gmail.com>
2019-05-04 08:54:41 +0900

    Add Repetition

--- SysLaBot/Repetition.cs
+++ SysLaBot/Repetition.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace SysLaBot
 {
-    class Repetition
+    /// <summary>
+    /// 繰り返しを定義します
+    /// </summary>
+    enum Repetition
     {
+        /// <summary>
+        /// 毎年、同じ月日に繰り返します
+        /// </summary>
+        EveryYear,
+        /// <summary>
+        /// 毎月、同じ日に繰り返します
+        /// </summary>
+        EveryMonth,
+        /// <summary>
+        /// 毎週、同じ曜日に繰り返します
+        /// </summary>
+        EveryWeek,
+        /// <summary>
+        /// 毎日、同じ時刻に繰り返します
+        /// </summary>
+        EveryDay,
+        /// <summary>
+        /// 一度きりです、まるで人生のように
+        /// </summary>
+        OnceOnly,
     }
 }
